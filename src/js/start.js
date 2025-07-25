a59ff820f67d913d7b52af1231a35a2f51fd1c8e
Steve De Jonghe <seutje@gmail.com>
2025-07-25 10:11:40 +0200

    Combine start and game screens (#27)

--- src/js/start.js
+++ src/js/start.js
@@ -2,9 +2,15 @@ const seedInput = document.getElementById('seedInput');
 const randomBtn = document.getElementById('randomBtn');
 const startBtn  = document.getElementById('startBtn');
 
-randomBtn.onclick = () => seedInput.value = Math.floor(Math.random() * 999999);
-startBtn.onclick  = () => {
+randomBtn.onclick = () => {
+  seedInput.value = Math.floor(Math.random() * 999999);
+};
+
+startBtn.onclick = async () => {
   const seed = seedInput.value || 12345;
   sessionStorage.setItem('qantSeed', seed);
-  location.href = 'play.html';
+  document.getElementById('startScreen').classList.add('hidden');
+  ['gameArea', 'ui-team-0', 'ui-team-1', 'ui-team-2', 'ui-team-3']
+    .forEach(id => document.getElementById(id).classList.remove('hidden'));
+  await import('./play.js');
 };
