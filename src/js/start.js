268bf73a602ba7ea638b6879a1341ab6e73ef00e
Steve De Jonghe <seutje@gmail.com>
2025-07-25 12:33:58 +0200

    Restart demo on seed change (#30)

--- src/js/start.js
+++ src/js/start.js
@@ -2,6 +2,17 @@ const seedInput = document.getElementById('seedInput');
 const randomBtn = document.getElementById('randomBtn');
 const startBtn  = document.getElementById('startBtn');
 
+// use any stored seed on reloads
+seedInput.value = sessionStorage.getItem('qantSeed') || '';
+
+function updateSeedAndRestart() {
+  const seed = seedInput.value || 12345;
+  sessionStorage.setItem('qantSeed', seed);
+  if (window.demoMode) {
+    location.reload();
+  }
+}
+
 // Show a demo match using AI for all teams
 window.demoMode = true;
 ['gameArea', 'ui-team-0', 'ui-team-1', 'ui-team-2', 'ui-team-3']
@@ -10,7 +21,10 @@ import('./play.js');
 
 randomBtn.onclick = () => {
   seedInput.value = Math.floor(Math.random() * 999999);
+  updateSeedAndRestart();
 };
+
+seedInput.addEventListener('change', updateSeedAndRestart);
 
 startBtn.onclick = async () => {
   window.demoMode = false;
