7519ed0597cce7015333be90d8b4b27937c6af01
Steve De Jonghe <seutje@gmail.com>
2025-07-26 07:27:13 +0200

    Orient ants towards target during combat (#39)

--- tests/ant.test.js
+++ tests/ant.test.js
@@ -8,3 +8,17 @@ test('ant attack does not reduce hp below zero', () => {
   attacker.attack(target);
   expect(target.hp).toBe(0);
 });
+
+test('ant faces target when attacking', () => {
+  gameState.ants = [];
+  const attacker = new Ant('private', 0, 0, 0);
+  const target = new Ant('worker', 1, 0.8, 0);
+  gameState.ants.push(attacker, target);
+  attacker.state = 'defending';
+  attacker.target = target;
+
+  attacker.update(1, [], []);
+
+  const expectedDir = Math.atan2(target.y - attacker.y, target.x - attacker.x);
+  expect(attacker.dir).toBeCloseTo(expectedDir);
+});
